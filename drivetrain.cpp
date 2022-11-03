1cc20497b4b4f9d529ccda61fed3cb34b11eac42
Saral Tayal <saraltayal@Sarals-MacBook-Air.local>
2022-11-03 09:04:54 -0400

    Fixed Servo jitter

--- drivetrain.cpp
+++ drivetrain.cpp
@@ -8,12 +8,24 @@ static Servo servo1;
 static Servo servo2;
 
 void drivetrainSetSpeed(uint8_t speed1, uint8_t speed2) {
-    if (speed1 > maxSpeed) {
-        speed1 = zeroSpeed;
+    // primitive speed clamping 
+    speed1 = min(max(speed1, zeroSpeed), maxSpeed);
+    speed2 = min(max(speed2, zeroSpeed), maxSpeed);
+
+    // turn off the servos with zero speed
+    if (speed1 == zeroSpeed) {
+        servo1.detach();
+    } else {
+        servo1.attach(SERVO1_PIN);
     }
-    if (speed2 > maxSpeed) {
-        speed2 = zeroSpeed;
+
+    if (speed2 == zeroSpeed) {
+        servo2.detach();
+    } else {
+        servo2.attach(SERVO2_PIN);
     }
+
+
     char debugStr[50];
     sprintf(debugStr, "Speed1, Speed2:\t %d, %d\n", speed1, speed2);
     Serial.println(debugStr);
@@ -22,6 +34,8 @@ void drivetrainSetSpeed(uint8_t speed1, uint8_t speed2) {
     servo2.write(speed2);
 }
 
+// not needed since this will attach and then detach the servos instantly
+// but it's here for completeness
 void drivetrainInit(void) {
     servo1.attach(SERVO1_PIN);
     servo2.attach(SERVO2_PIN);
